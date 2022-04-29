f7bea4c9ad3905c8b3e072ba81a4e73dd5a12e95
yayoflake <104496981+yayoflake@users.noreply.github.com>
2022-04-30 02:44:35 +0900

    sd ad

---
+++ src/aaa.js
@@ -0,0 +1,27 @@
+const xhrButton = document.getElementById("xhr-button");
+const fetchButton = document.getElementById("fetch-button");
+const xhrP = document.getElementById("xhr-p");
+const fetchP = document.getElementById("fetch-p");
+
+const url = "https://yayoflake.github.io/sandtest/aaa.html";
+
+const data = "data";
+
+const onClickFetch = () =>
+  fetch(url)
+    .then((res) => res.json())
+    .then((resJson) => {
+      fetchP.textContent = JSON.stringify(resJson);
+    });
+
+const onClickXhr = () => {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", url);
+  xhr.onload = () => {
+    xhrP.textContent = xhr.responseText;
+  };
+  xhr.send();
+};
+
+xhrButton.addEventListener("click", onClickXhr);
+fetchButton.addEventListener("click", onClickFetch);
