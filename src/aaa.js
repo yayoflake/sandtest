75b8dd36ea0caa0457efa932a61a5f52044551c1
yayoflake <104496981+yayoflake@users.noreply.github.com>
2022-04-30 22:47:12 +0900

    ㅇ

--- src/aaa.js
+++ src/aaa.js
@@ -1,19 +1,10 @@
 const xhrButton = document.getElementById("xhr-button");
-const fetchButton = document.getElementById("fetch-button");
 const xhrP = document.getElementById("xhr-p");
-const fetchP = document.getElementById("fetch-p");
 
 //const url = "https://yayoflake.github.io/sandtest/aaa.html";
 const url = "https://yayoflake.github.io/sandtest/result.html?id=ss&password=";
 
 const data = "data";
-
-const onClickFetch = () =>
-  fetch(url)
-    .then((res) => res.json())
-    .then((resJson) => {
-      fetchP.textContent = JSON.stringify(resJson);
-    });
 
 const onClickXhr = () => {
   const xhr = new XMLHttpRequest();
@@ -25,4 +16,3 @@ const onClickXhr = () => {
 };
 
 xhrButton.addEventListener("click", onClickXhr);
-fetchButton.addEventListener("click", onClickFetch);
--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
 const xhrButton = document.getElementById("xhr-button");
-const fetchButton = document.getElementById("fetch-button");
 const xhrP = document.getElementById("xhr-p");
-const fetchP = document.getElementById("fetch-p");
 
 const url = "https://learnwebcode.github.io/json-example/animals-1.json";
-
-const onClickFetch = () =>
-  fetch(url)
-    .then((res) => res.json())
-    .then((resJson) => {
-      fetchP.textContent = JSON.stringify(resJson);
-    });
 
 const onClickXhr = () => {
   const xhr = new XMLHttpRequest();
@@ -22,4 +13,3 @@ const onClickXhr = () => {
 };
 
 xhrButton.addEventListener("click", onClickXhr);
-fetchButton.addEventListener("click", onClickFetch);
